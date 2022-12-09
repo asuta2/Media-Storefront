a044a7e44ac79f94c0f69f7e417e7d52d1fae297
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-09 22:52:55 +0100

    Implementirani getteri za svaki atribut unutar Media klase.

--- src/main/java/ba/unsa/etf/rpr/mn/Media.java
+++ src/main/java/ba/unsa/etf/rpr/mn/Media.java
@@ -4,6 +4,23 @@ public class Media {
     private int idMedia;
     private String mediaName;
     private String mediaCreator;
+
+    public int getIdMedia() {
+        return idMedia;
+    }
+
+    public String getMediaName() {
+        return mediaName;
+    }
+
+    public String getMediaCreator() {
+        return mediaCreator;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
     private int typeId;
 
 }
