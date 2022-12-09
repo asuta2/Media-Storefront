8132f106f6bd6239219f2f1cdd4bd6656e7e805c
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-09 22:49:55 +0100

    Dodani atributi u klasu borrows i uključena biblioteka java.util.Date zbog atributa datuma.

--- src/main/java/ba/unsa/etf/rpr/mn/borrows.java
+++ src/main/java/ba/unsa/etf/rpr/mn/borrows.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.mn;
 
+import java.util.Date;
+
 public class borrows {
+    private int borrowsId;
+    private int userId;
+    private int mediaId;
+    private Date takeDate;
+    private Date broughtDate;
 }
