ff7baead3346505b4c2ef0d478549012a15fefc9
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-09 23:07:41 +0100

    Dodani intefejsi za Media i Types klase

---
+++ src/main/java/ba/unsa/etf/rpr/dao/MediaDao.java
@@ -0,0 +1,6 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.mn.Media;
+
+public interface MediaDao extends dao<Media>{
+}
---
+++ src/main/java/ba/unsa/etf/rpr/dao/TypesDao.java
@@ -0,0 +1,5 @@
+package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.mn.Types;
+
+public interface TypesDao extends dao<Types>{
+}
