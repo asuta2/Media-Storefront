0f9b8a05cef5597e0d66ba61d51dbde7e9a10cf9
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-09 22:55:58 +0100

    Implementirani setteri za svaki atribut unutar Types klase.

--- src/main/java/ba/unsa/etf/rpr/mn/Types.java
+++ src/main/java/ba/unsa/etf/rpr/mn/Types.java
@@ -3,4 +3,20 @@ package ba.unsa.etf.rpr.mn;
 public class Types {
     private int idTypes;
     private String typeName;
+
+    public int getIdTypes() {
+        return idTypes;
+    }
+
+    public void setIdTypes(int idTypes) {
+        this.idTypes = idTypes;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
 }
