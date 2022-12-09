8d2351c01fef3f97867cb2b20117b12bfe94a6c6
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-09 22:55:35 +0100

    Implementirani setteri za svaki atribut unutar borrows klase.

--- src/main/java/ba/unsa/etf/rpr/mn/borrows.java
+++ src/main/java/ba/unsa/etf/rpr/mn/borrows.java
@@ -8,4 +8,44 @@ public class borrows {
     private int mediaId;
     private Date takeDate;
     private Date broughtDate;
+
+    public int getBorrowsId() {
+        return borrowsId;
+    }
+
+    public void setBorrowsId(int borrowsId) {
+        this.borrowsId = borrowsId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(int mediaId) {
+        this.mediaId = mediaId;
+    }
+
+    public Date getTakeDate() {
+        return takeDate;
+    }
+
+    public void setTakeDate(Date takeDate) {
+        this.takeDate = takeDate;
+    }
+
+    public Date getBroughtDate() {
+        return broughtDate;
+    }
+
+    public void setBroughtDate(Date broughtDate) {
+        this.broughtDate = broughtDate;
+    }
 }
