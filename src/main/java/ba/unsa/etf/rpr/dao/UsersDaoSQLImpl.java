1c08fdd6c0ea3278997d6522573dd763b7391e9a
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-10 14:41:17 +0100

    Implementirana implementacija UsersDAO i metoda za dodavanje u bazu podataka.

---
+++ src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
@@ -0,0 +1,60 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.mn.Users;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+public class UsersDaoSQLImpl implements UsersDao{
+    private Connection conn;
+    static final String DB_URL = "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7583670";
+    static final String USER = "sql7583670";
+    static final String PASS = "KVxzvQ1ssg";
+
+    public UsersDaoSQLImpl() {
+        try{
+            this.conn= DriverManager.getConnection(DB_URL,USER,PASS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Users getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Users add(Users item) {
+        String upit = "INSERT INTO Users(email,password,Username) VALUES(?,?,?)";
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(upit, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, item.getEmail());
+            stmt.setString(2, item.getPassword());
+            stmt.setString(3, item.getUsername());
+            stmt.executeUpdate();
+            return item;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Users update(Users item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Users> getAll() {
+        return null;
+    }
+}
