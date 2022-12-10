7304569034c08f3754c6ea1b92195a38d194a2f0
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-10 17:51:52 +0100

    Interfejs za metodu vraćanja svih zaduženja po id-u.

--- src/main/java/ba/unsa/etf/rpr/dao/borrowsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/borrowsDao.java
@@ -2,5 +2,8 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.mn.borrows;
 
+import java.util.List;
+
 public interface borrowsDao extends dao<borrows>{
+    List<borrows> getAllBorrowsById(int id);
 }
