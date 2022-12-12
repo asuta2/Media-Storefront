9ce0954be8a86cceaff0f286b2851145754738a4
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-12 21:29:55 +0100

    Removed borrowsDao.java

--- src/main/java/ba/unsa/etf/rpr/dao/borrowsDao.java
+++
@@ -1,9 +0,0 @@
-package ba.unsa.etf.rpr.dao;
-
-import ba.unsa.etf.rpr.mn.borrows;
-
-import java.util.List;
-
-public interface borrowsDao extends dao<borrows>{
-    List<borrows> getAllBorrowsById(int id);
-}
