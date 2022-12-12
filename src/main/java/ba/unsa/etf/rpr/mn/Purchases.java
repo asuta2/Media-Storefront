495d18961158be3b1e6a44107695fe32d2a6de29
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-12 20:46:42 +0100

    Preimenovano u Purchases te izvršene izmjene na određenim atributima da ogleda ove promjene.

---
+++ src/main/java/ba/unsa/etf/rpr/mn/Purchases.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.rpr.mn;
+
+import java.util.Date;
+
+public class Purchases {
+    private int purchasesId;
+    private int userId;
+    private int mediaId;
+    private Date boughtDate;
+
+
+    public int getPurchasesId() {
+        return purchasesId;
+    }
+
+    public void setPurchasesId(int purchasesId) {
+        this.purchasesId = purchasesId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(int mediaId) {
+        this.mediaId = mediaId;
+    }
+
+
+
+    public Date getBoughtDate() {
+        return boughtDate;
+    }
+
+    public void setBoughtDate(Date boughtDate) {
+        this.boughtDate = boughtDate;
+    }
+}
