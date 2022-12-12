e7548b0169555bfa9bd794eb5153aea44e56b149
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-12 21:33:50 +0100

    Dodan TypesDaoSQLImpl

---
+++ src/main/java/ba/unsa/etf/rpr/dao/TypesDaoSQLImpl.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.mn.Types;
+
+import java.util.List;
+
+public class TypesDaoSQLImpl implements TypesDao {
+
+    @Override
+    public Types getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Types add(Types item) {
+        return null;
+    }
+
+    @Override
+    public Types update(Types item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Types> getAll() {
+        return null;
+    }
+}
