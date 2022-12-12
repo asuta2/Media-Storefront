1b9e384c01d20379c1e6b6ac5370b87bf2c06bf9
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-12 20:48:34 +0100

    Ažurirana UsersDaoSQLImpl klasa da ogleda promjene u postavci baze podataka.

--- src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
@@ -34,6 +34,10 @@ public class UsersDaoSQLImpl implements UsersDao{
                 user.setUsername(rs.getString("Username"));
                 user.setPassword(rs.getString("password"));
                 user.setEmail(rs.getString("email"));
+                user.setBirthDate(rs.getDate("BirthDate"));
+                user.setBalance(rs.getDouble("Balance"));
+                user.setPrivilegeLevel(rs.getString("PrivilegeLevel"));
+
                 rs.close();
                 return user;
             }
@@ -46,12 +50,15 @@ public class UsersDaoSQLImpl implements UsersDao{
 
     @Override
     public Users add(Users item) {
-        String upit = "INSERT INTO Users(email,password,Username) VALUES(?,?,?)";
+        String upit = "INSERT INTO Users(email,password,Username,BirthDate,Balance,PPrivilegeLevel) VALUES(?,?,?,?,?,?)";
         try{
             PreparedStatement stmt = this.conn.prepareStatement(upit, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, item.getEmail());
             stmt.setString(2, item.getPassword());
             stmt.setString(3, item.getUsername());
+            stmt.setDate(4, item.getBirthDate());
+            stmt.setDouble(5, item.getBalance());
+            stmt.setString(6, item.getPrivilegeLevel());
             stmt.executeUpdate();
             return item;
         } catch (Exception e) {
@@ -92,6 +99,9 @@ public class UsersDaoSQLImpl implements UsersDao{
                 user.setUsername(rs.getString("Username"));
                 user.setEmail(rs.getString("email"));
                 user.setPassword(rs.getString("password"));
+                user.setBirthDate(rs.getDate("BirthDate"));
+                user.setBalance(rs.getDouble("Balance"));
+                user.setPrivilegeLevel(rs.getString("PrivilegeLevel"));
                 ispis.add(user);
             }
             rs.close();
