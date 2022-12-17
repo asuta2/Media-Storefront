d93ff559fd6ddb4655c437cb56fdc40c136d7249
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-17 23:30:22 +0100

    Promjena u korištenja metode patternMatches za provjeru formatiranja mejla.

--- src/main/java/ba/unsa/etf/rpr/controllers/registerController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/registerController.java
@@ -3,8 +3,6 @@ package ba.unsa.etf.rpr.controllers;
 import ba.unsa.etf.rpr.dao.UsersDao;
 import ba.unsa.etf.rpr.dao.UsersDaoSQLImpl;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -22,7 +20,7 @@ public class registerController {
     public Label errorLabel2;
     public Label errorLabel3;
     public Label errorLabel4;
-    private UsersDao usersDao = new UsersDaoSQLImpl();
+    private final UsersDao usersDao = new UsersDaoSQLImpl();
     private SimpleStringProperty username;
     private SimpleStringProperty password;
     private SimpleStringProperty email;
@@ -40,93 +38,81 @@ public class registerController {
         pass.textProperty().bindBidirectional(password);
         emailField.textProperty().bindBidirectional(email);
         emailConfirmField.textProperty().bindBidirectional(emailConfirm);
-        usernameField.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                if(usernameField.getText().length() > 20){
-                    usernameField.getStyleClass().removeAll("successCode");
-                    usernameField.getStyleClass().add("errorCode");
-                    errorLabel1.setText("Username is too long.");
-                }
-                else if(usernameField.getText().trim().isEmpty()){
-                    usernameField.getStyleClass().removeAll("successCode");
-                    usernameField.getStyleClass().add("errorCode");
-                    errorLabel1.setText("Username is empty.");
-                }else if(usersDao.checkUsername(usernameField.getText())){
-                    usernameField.getStyleClass().removeAll("successCode");
-                    usernameField.getStyleClass().add("errorCode");
-                    errorLabel1.setText("Username is taken.");
-                }
-                else{
-                    usernameField.getStyleClass().removeAll("errorCode");
-                    errorLabel1.setText("");
-                    usernameField.getStyleClass().add("successCode");
-                }
+        usernameField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(usernameField.getText().length() > 20){
+                usernameField.getStyleClass().removeAll("successCode");
+                usernameField.getStyleClass().add("errorCode");
+                errorLabel1.setText("Username is too long.");
+            }
+            else if(usernameField.getText().trim().isEmpty()){
+                usernameField.getStyleClass().removeAll("successCode");
+                usernameField.getStyleClass().add("errorCode");
+                errorLabel1.setText("Username is empty.");
+            }else if(usersDao.checkUsername(usernameField.getText())){
+                usernameField.getStyleClass().removeAll("successCode");
+                usernameField.getStyleClass().add("errorCode");
+                errorLabel1.setText("Username is taken.");
+            }
+            else{
+                usernameField.getStyleClass().removeAll("errorCode");
+                errorLabel1.setText("");
+                usernameField.getStyleClass().add("successCode");
             }
         });
-        pass.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                if(pass.getText().length() > 20){
-                    pass.getStyleClass().removeAll("successCode");
-                    pass.getStyleClass().add("errorCode");
-                    errorLabel4.setText("Password is too long.");
-                }else if(pass.getText().length() < 8){
-                    pass.getStyleClass().removeAll("successCode");
-                    pass.getStyleClass().add("errorCode");
-                    errorLabel4.setText("Password is too short.");
-                }else if(pass.getText().trim().isEmpty()){
-                    pass.getStyleClass().removeAll("successCode");
-                    pass.getStyleClass().add("errorCode");
-                    errorLabel4.setText("Password is empty.");
-                }
-                else{
-                    pass.getStyleClass().remove("errorCode");
-                    errorLabel4.setText("");
-                    pass.getStyleClass().add("successCode");
-                }
+        pass.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(pass.getText().length() > 20){
+                pass.getStyleClass().removeAll("successCode");
+                pass.getStyleClass().add("errorCode");
+                errorLabel4.setText("Password is too long.");
+            }else if(pass.getText().length() < 8){
+                pass.getStyleClass().removeAll("successCode");
+                pass.getStyleClass().add("errorCode");
+                errorLabel4.setText("Password is too short.");
+            }else if(pass.getText().trim().isEmpty()){
+                pass.getStyleClass().removeAll("successCode");
+                pass.getStyleClass().add("errorCode");
+                errorLabel4.setText("Password is empty.");
+            }
+            else{
+                pass.getStyleClass().remove("errorCode");
+                errorLabel4.setText("");
+                pass.getStyleClass().add("successCode");
             }
         });
-        emailField.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                if(emailField.getText().length() > 50){
-                    emailField.getStyleClass().add("errorCode");
-                    errorLabel2.setText("Email is too long.");
-                }
-                else if(emailField.getText().trim().isEmpty()){
-                    emailField.getStyleClass().add("errorCode");
-                    errorLabel2.setText("Email is empty.");
-                }else if(!Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$", emailField.getText())){
-                    emailField.getStyleClass().add("errorCode");
-                    errorLabel2.setText("Email is not valid.");
-                }else if(usersDao.checkEmail(emailField.getText())){
-                    emailField.getStyleClass().add("errorCode");
-                    errorLabel2.setText("Email is taken.");
-                }else{
-                    emailField.getStyleClass().remove("errorCode");
-                    emailField.getStyleClass().add("successCode");
-                    errorLabel2.setText("");
-                }
+        emailField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(emailField.getText().length() > 50){
+                emailField.getStyleClass().add("errorCode");
+                errorLabel2.setText("Email is too long.");
+            }
+            else if(emailField.getText().trim().isEmpty()){
+                emailField.getStyleClass().add("errorCode");
+                errorLabel2.setText("Email is empty.");
+            }else if(!patternMatches(emailField.getText(), "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")){
+                emailField.getStyleClass().add("errorCode");
+                errorLabel2.setText("Email is not valid.");
+            }else if(usersDao.checkEmail(emailField.getText())){
+                emailField.getStyleClass().add("errorCode");
+                errorLabel2.setText("Email is taken.");
+            }else{
+                emailField.getStyleClass().remove("errorCode");
+                emailField.getStyleClass().add("successCode");
+                errorLabel2.setText("");
             }
         });
-        emailConfirmField.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                if(emailConfirmField.getText().equals(emailField.getText())){
-                    emailConfirmField.getStyleClass().removeAll("errorCode");
-                    errorLabel3.setText("");
-                    emailConfirmField.getStyleClass().add("successCode");
-                }else if(emailConfirmField.getText().trim().isEmpty()) {
-                    emailConfirmField.getStyleClass().removeAll("successCode");
-                    emailConfirmField.getStyleClass().add("errorCode");
-                    errorLabel3.setText("Email is empty.");
-                }
-                else{
-                    emailConfirmField.getStyleClass().removeAll("successCode");
-                    emailConfirmField.getStyleClass().add("errorCode");
-                    errorLabel3.setText("Emails don't match.");
-                }
+        emailConfirmField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(emailConfirmField.getText().equals(emailField.getText())){
+                emailConfirmField.getStyleClass().removeAll("errorCode");
+                errorLabel3.setText("");
+                emailConfirmField.getStyleClass().add("successCode");
+            }else if(emailConfirmField.getText().trim().isEmpty()) {
+                emailConfirmField.getStyleClass().removeAll("successCode");
+                emailConfirmField.getStyleClass().add("errorCode");
+                errorLabel3.setText("Email is empty.");
+            }
+            else{
+                emailConfirmField.getStyleClass().removeAll("successCode");
+                emailConfirmField.getStyleClass().add("errorCode");
+                errorLabel3.setText("Emails don't match.");
             }
         });
     }
@@ -139,5 +125,6 @@ public class registerController {
                 .matcher(emailAddress)
                 .matches();
     }
+
 
 }
