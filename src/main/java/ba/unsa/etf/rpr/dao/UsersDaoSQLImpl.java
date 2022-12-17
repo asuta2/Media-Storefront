87fa44ef58d58d213b31f609b2eb61011da4a29e
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-17 18:50:47 +0100

    Implementacija metode za provjeru učestalosti korisnika u bazi podataka.

--- src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
@@ -6,15 +6,15 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UsersDaoSQLImpl implements UsersDao{
+public class UsersDaoSQLImpl implements UsersDao {
     private Connection conn;
     static final String DB_URL = "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7583670";
     static final String USER = "sql7583670";
     static final String PASS = "KVxzvQ1ssg";
 
     public UsersDaoSQLImpl() {
-        try{
-            this.conn= DriverManager.getConnection(DB_URL,USER,PASS);
+        try {
+            this.conn = DriverManager.getConnection(DB_URL, USER, PASS);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -25,10 +25,9 @@ public class UsersDaoSQLImpl implements UsersDao{
         String upit = "Select * from Users where idUsers = ?";
         try {
             PreparedStatement stmt = this.conn.prepareStatement(upit);
-            stmt.setInt(1,id);
+            stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
-            if(rs.next())
-            {
+            if (rs.next()) {
                 Users user = new Users();
                 user.setIdUsers(rs.getInt("idUsers"));
                 user.setUsername(rs.getString("Username"));
@@ -51,7 +50,7 @@ public class UsersDaoSQLImpl implements UsersDao{
     @Override
     public Users add(Users item) {
         String upit = "INSERT INTO Users(email,password,Username,BirthDate,Balance,PPrivilegeLevel) VALUES(?,?,?,?,?,?)";
-        try{
+        try {
             PreparedStatement stmt = this.conn.prepareStatement(upit, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, item.getEmail());
             stmt.setString(2, item.getPassword());
@@ -75,9 +74,9 @@ public class UsersDaoSQLImpl implements UsersDao{
     @Override
     public void delete(int id) {
         String upit = "DELETE FROM Users where idUsers = ?";
-        try{
+        try {
             PreparedStatement stmt = this.conn.prepareStatement(upit, Statement.RETURN_GENERATED_KEYS);
-            stmt.setInt(1,id);
+            stmt.setInt(1, id);
             stmt.executeUpdate();
 
         } catch (Exception e) {
@@ -89,11 +88,10 @@ public class UsersDaoSQLImpl implements UsersDao{
     public List<Users> getAll() {
         String upit = "SELECT * FROM Users";
         List<Users> ispis = new ArrayList<>();
-        try{
+        try {
             PreparedStatement stmt = this.conn.prepareStatement(upit);
             ResultSet rs = stmt.executeQuery();
-            while (rs.next())
-            {
+            while (rs.next()) {
                 Users user = new Users();
                 user.setIdUsers(rs.getInt("idUsers"));
                 user.setUsername(rs.getString("Username"));
@@ -109,5 +107,20 @@ public class UsersDaoSQLImpl implements UsersDao{
             e.printStackTrace();
         }
         return ispis;
+    }
+
+    @Override
+    public boolean checkUser(String email, String password) {
+        String upit = "SELECT * FROM Users where email = ? and password = ?";
+        try {
+            PreparedStatement stmt = this.conn.prepareStatement(upit);
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
