ce7c550fbf51a573ce944f6eb77022c0b19697c2
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-17 18:43:26 +0100

    Dodani atributi za ostatak prozora i osnovni regex za email i password provjeru

--- src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
@@ -7,18 +7,25 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Controller  {
     public Hyperlink RegisterBtn;
     public TextField UsernameField;
+    public PasswordField PasswordField;
+    public Button loginButton;
+    public Label errorLabel;
     private SimpleStringProperty owo;
-    //public PasswordField PasswordField;
+
+    public static boolean patternMatches(String emailAddress, String regexPattern) {
+        return Pattern.compile(regexPattern)
+                .matcher(emailAddress)
+                .matches();
+    }
 
     public Controller(){
         owo = new SimpleStringProperty("");
@@ -26,6 +33,30 @@ public class Controller  {
     @FXML
     public void initialize(){
         UsernameField.textProperty().bindBidirectional(owo);
+    }
+    public void checkSignIn(ActionEvent actionEvent) {
+        if(UsernameField.getText().isEmpty() || PasswordField.getText().isEmpty()){
+            System.out.println("Niste unijeli sve podatke!");
+            errorLabel.setText("Please check your password and account name and try again.");
+            UsernameField.getStyleClass().add("errorCode");
+            PasswordField.getStyleClass().add("errorCode");
+        }
+        else if(!patternMatches(UsernameField.getText(), "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")){
+            System.out.println("Neispravan format email adrese!");
+            errorLabel.setText("Please check your password and account name and try again.");
+            UsernameField.getStyleClass().add("errorCode");
+            PasswordField.getStyleClass().add("errorCode");
+        } else if (PasswordField.getText().length() < 8){
+            System.out.println("Lozinka mora imati najmanje 8 znakova!");
+            errorLabel.setText("Please check your password and account name and try again.");
+            UsernameField.getStyleClass().add("errorCode");
+            PasswordField.getStyleClass().add("errorCode");
+        } else {
+            UsernameField.getStyleClass().remove("errorCode");
+            PasswordField.getStyleClass().remove("errorCode");
+            errorLabel.setText("");
+            System.out.println("Uspješno ste se prijavili!");
+        }
     }
 
     public void hyperClick(ActionEvent actionEvent) {
