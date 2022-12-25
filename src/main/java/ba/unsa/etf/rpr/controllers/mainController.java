74dd1102eb896686c9ad40a927ba82a17b23aacb
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-25 20:06:03 +0100

    Dodana akcija pri pritiskanju dugmeta za Library.

--- src/main/java/ba/unsa/etf/rpr/controllers/mainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/mainController.java
@@ -1,11 +1,18 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.MediaManager;
+import ba.unsa.etf.rpr.business.PurchasesManager;
 import ba.unsa.etf.rpr.mn.Media;
+import ba.unsa.etf.rpr.mn.Purchases;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class mainController {
 
@@ -13,7 +20,7 @@ public class mainController {
     public Button addFundsButton;
     public ListView<Media> mediaList;
     public Button usernameButton;
-    private MediaManager mediaManager = new MediaManager();
+    private final MediaManager mediaManager = new MediaManager();
     @FXML
     public void initialize() {
         try{
@@ -36,4 +43,19 @@ public class mainController {
         }
     }
 
+
+    public void libraryButtonClick(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/library.fxml"));
+            Parent root = loader.load();
+            libraryController nv = loader.getController();
+            nv.welcomeLabel.setText("Welcome to your Library, " + usernameButton.getText());
+            stage.setTitle("Library");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
