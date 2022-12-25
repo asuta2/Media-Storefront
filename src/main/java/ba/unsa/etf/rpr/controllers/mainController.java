1fe6518342bad3ede896d7f424a7438bbace6089
EkkoOwO <asuta2@etf.unsa.ba>
2022-12-25 13:04:29 +0100

    Dodan mainController za glavni prozor programa.

--- src/main/java/ba/unsa/etf/rpr/controllers/mainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/mainController.java
@@ -1,5 +1,33 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.MediaManager;
+import ba.unsa.etf.rpr.mn.Media;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class mainController {
+
+    public Button myLibraryButton;
+    public Button addFundsButton;
+    public ListView<Media> mediaList;
+    private MediaManager mediaManager = new MediaManager();
+    @FXML
+    public void initialize() {
+        try{
+            refreshList();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void refreshList() {
+        try {
+            mediaList.setItems(FXCollections.observableList(mediaManager.getAll()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
