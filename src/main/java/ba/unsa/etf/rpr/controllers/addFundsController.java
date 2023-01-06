425c12c15a04d98155183ede31c8d4f57a8f9f75
EkkoOwO <asuta2@etf.unsa.ba>
2023-01-06 09:10:06 +0100

    Dodane akcije pri pritiskanju dugmeta za dodavanje prihoda u korisnički bilans.

--- src/main/java/ba/unsa/etf/rpr/controllers/addFundsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/addFundsController.java
@@ -1,7 +1,40 @@
 package ba.unsa.etf.rpr.controllers;
 
-import java.awt.*;
+import ba.unsa.etf.rpr.business.UsersManager;
+import javafx.event.ActionEvent;
 
 public class addFundsController {
-    public Label welcomeLabel;
+    UsersManager usersManager = new UsersManager();
+    public javafx.scene.control.Label welcome;
+
+    public void Add5(ActionEvent actionEvent) {
+        //Adds 5$ to the balance of the user
+        usersManager.addFunds(5);
+        usersManager.update(UsersManager.getCurrentUser());
+        welcome.setText("Welcome " + UsersManager.getCurrentUser().getUsername() + " your balance is " + UsersManager.getCurrentUser().getBalance() + "$");
+
+    }
+
+    public void Add10(ActionEvent actionEvent) {
+        //Adds 10$ to the balance of the user
+        usersManager.addFunds(10);
+        usersManager.update(UsersManager.getCurrentUser());
+        welcome.setText("Welcome " + UsersManager.getCurrentUser().getUsername() + " your balance is " + UsersManager.getCurrentUser().getBalance() + "$");
+
+    }
+
+    public void Add30(ActionEvent actionEvent) {
+        //Adds 30$ to the balance of the user
+        usersManager.addFunds(30);
+        usersManager.update(UsersManager.getCurrentUser());
+        welcome.setText("Welcome " + UsersManager.getCurrentUser().getUsername() + " your balance is " + UsersManager.getCurrentUser().getBalance() + "$");
+
+    }
+
+    public void Add50(ActionEvent actionEvent) {
+        //Adds 50$ to the balance of the user
+        usersManager.addFunds(50);
+        usersManager.update(UsersManager.getCurrentUser());
+        welcome.setText("Welcome " + UsersManager.getCurrentUser().getUsername() + " your balance is " + UsersManager.getCurrentUser().getBalance() + "$");
+    }
 }
