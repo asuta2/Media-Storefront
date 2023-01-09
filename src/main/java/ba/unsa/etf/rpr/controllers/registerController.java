0bfb1ace4d81cad9aa0c4e84638bc5c1e1f257c2
EkkoOwO <asuta2@etf.unsa.ba>
2023-01-09 12:54:39 +0100

    Dodane izmjene u signup dugmetu da bude disabled po defaultu.

--- src/main/java/ba/unsa/etf/rpr/controllers/registerController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/registerController.java
@@ -10,10 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.util.regex.Pattern;
@@ -28,6 +25,7 @@ public class registerController {
     public Label errorLabel3;
     public Label errorLabel4;
     public CheckBox checker;
+    public Button signupButton;
     private SimpleStringProperty username;
     private SimpleStringProperty password;
     private SimpleStringProperty email;
@@ -46,26 +44,31 @@ public class registerController {
         pass.textProperty().bindBidirectional(password);
         emailField.textProperty().bindBidirectional(email);
         emailConfirmField.textProperty().bindBidirectional(emailConfirm);
+        signupButton.setDisable(true);
 
         usernameField.textProperty().addListener((observable, oldValue, newValue) -> {
             if(usernameField.getText().length() > 20){
                 usernameField.getStyleClass().removeAll("successCode");
                 usernameField.getStyleClass().add("errorCode");
                 errorLabel1.setText("Username is too long.");
+                signupButton.setDisable(true);
             }
             else if(usernameField.getText().trim().isEmpty()){
                 usernameField.getStyleClass().removeAll("successCode");
                 usernameField.getStyleClass().add("errorCode");
                 errorLabel1.setText("Username is empty.");
+                signupButton.setDisable(true);
             }else if(usersManager.checkUsername(usernameField.getText())){
                 usernameField.getStyleClass().removeAll("successCode");
                 usernameField.getStyleClass().add("errorCode");
                 errorLabel1.setText("Username is taken.");
+                signupButton.setDisable(true);
             }
             else{
                 usernameField.getStyleClass().removeAll("errorCode");
                 errorLabel1.setText("");
                 usernameField.getStyleClass().add("successCode");
+                signupButton.setDisable(false);
             }
         });
         pass.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -73,19 +76,23 @@ public class registerController {
                 pass.getStyleClass().removeAll("successCode");
                 pass.getStyleClass().add("errorCode");
                 errorLabel4.setText("Password is too long.");
+                signupButton.setDisable(true);
             }else if(pass.getText().length() < 8){
                 pass.getStyleClass().removeAll("successCode");
                 pass.getStyleClass().add("errorCode");
                 errorLabel4.setText("Password is too short.");
+                signupButton.setDisable(true);
             }else if(pass.getText().trim().isEmpty()){
                 pass.getStyleClass().removeAll("successCode");
                 pass.getStyleClass().add("errorCode");
                 errorLabel4.setText("Password is empty.");
+                signupButton.setDisable(true);
             }
             else{
                 pass.getStyleClass().remove("errorCode");
                 errorLabel4.setText("");
                 pass.getStyleClass().add("successCode");
+                signupButton.setDisable(false);
             }
         });
         emailField.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -93,23 +100,28 @@ public class registerController {
                 emailField.getStyleClass().removeAll("successCode");
                 emailField.getStyleClass().add("errorCode");
                 errorLabel2.setText("Email is too long.");
+                signupButton.setDisable(true);
             }
             else if(emailField.getText().trim().isEmpty()){
                 emailField.getStyleClass().removeAll("successCode");
                 emailField.getStyleClass().add("errorCode");
                 errorLabel2.setText("Email is empty.");
+                signupButton.setDisable(true);
             }else if(!patternMatches(emailField.getText(), "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")){
                 emailField.getStyleClass().removeAll("successCode");
                 emailField.getStyleClass().add("errorCode");
                 errorLabel2.setText("Email is not valid.");
+                signupButton.setDisable(true);
             }else if(usersManager.checkEmail(emailField.getText())){
                 emailField.getStyleClass().removeAll("successCode");
                 emailField.getStyleClass().add("errorCode");
                 errorLabel2.setText("Email is taken.");
+                signupButton.setDisable(true);
             }else{
                 emailField.getStyleClass().removeAll("errorCode");
                 emailField.getStyleClass().add("successCode");
                 errorLabel2.setText("");
+                signupButton.setDisable(false);
             }
         });
         emailConfirmField.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -117,15 +129,18 @@ public class registerController {
                 emailConfirmField.getStyleClass().removeAll("errorCode");
                 errorLabel3.setText("");
                 emailConfirmField.getStyleClass().add("successCode");
+                signupButton.setDisable(false);
             }else if(emailConfirmField.getText().trim().isEmpty()) {
                 emailConfirmField.getStyleClass().removeAll("successCode");
                 emailConfirmField.getStyleClass().add("errorCode");
                 errorLabel3.setText("Email is empty.");
+                signupButton.setDisable(true);
             }
             else{
                 emailConfirmField.getStyleClass().removeAll("successCode");
                 emailConfirmField.getStyleClass().add("errorCode");
                 errorLabel3.setText("Emails don't match.");
+                signupButton.setDisable(true);
             }
         });
     }
@@ -137,6 +152,7 @@ public class registerController {
         }
         else{
             checker.getStyleClass().removeAll("errorCode");
+            signupButton.setDisable(false);
         }
         if(usernameField.getStyleClass().contains("successCode") && pass.getStyleClass().contains("successCode") && emailField.getStyleClass().contains("successCode") && emailConfirmField.getStyleClass().contains("successCode") && checker.isSelected()){
             Users temp = new Users();
@@ -146,6 +162,7 @@ public class registerController {
             temp.setPrivilegeLevel("user");
             temp.setBalance(0.0);
             usersManager.add(temp);
+            System.out.println("User added" + temp.getUsername() );
 
             //close window and go back to login
             Stage stage = (Stage) usernameField.getScene().getWindow();
