498e5cc91bc515116a4b0ba55db9784c82c0e77b
EkkoOwO <asuta2@etf.unsa.ba>
2023-01-11 15:59:32 +0100

    Promjenjena abstraktna metoda delete u svim instancama.

--- src/main/java/ba/unsa/etf/rpr/dao/dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/dao.java
@@ -6,6 +6,6 @@ public interface dao <T>{
     T getById(int id);
     T add(T item);
     T update(T item);
-    void delete(int id);
+    void delete(int id,String columnName);
     List<T> getAll();
 }
